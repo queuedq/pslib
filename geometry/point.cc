de38e86641d48afde813407b9a36170beeca272d
Dongkyu Han <queued37@gmail.com>
2020-10-05 19:51:41 +0900

    Test Point

--- geometry/point.cc
+++ geometry/point.cc
@@ -1,19 +1,27 @@
 #include <template.h>
 
-struct Point { lld x, y; };
-bool operator<(Point a, Point b) { return (pll){a.x, a.y} < (pll){b.x, b.y}; }
+const double PI = acos(-1);
+
+struct Point {
+  lld x, y;
+  Point(lld x, lld y): x(x), y(y) {}
+  double length();
+};
 bool operator==(Point a, Point b) { return a.x == b.x && a.y == b.y; }
+bool operator!=(Point a, Point b) { return !(a == b); }
+bool operator<(Point a, Point b) { return (pll){a.x, a.y} < (pll){b.x, b.y}; }
 Point operator+(Point a, Point b) { return {a.x + b.x, a.y + b.y}; }
 Point operator-(Point a, Point b) { return {a.x - b.x, a.y - b.y}; }
-Point operator*(lld a, Point b) { return {a * b.x, a * b.y}; }
+Point operator*(lld c, Point a) { return {c * a.x, c * a.y}; }
 lld operator*(Point a, Point b) { return a.x * b.x + a.y * b.y; }
-ostream& operator<<(ostream& os, Point a) { os << "{" << a.x << "," << a.y << "}"; return os; }
+ostream& operator<<(ostream& os, Point a) { os << "(" << a.x << ", " << a.y << ")"; return os; }
 
 lld cross(Point a, Point b) { return a.x * b.y - a.y * b.x; }
 lld ccw(Point a, Point b, Point c) { return cross(b - a, c - a); }
-double length(Point a) { return sqrt(a * a); }
+
+double Point::length() { return sqrt(*this * *this); }
 double angle(Point a, Point b) {
-  double c = a * b / length(a) / length(b);
-  double s = cross(a, b) / length(a) / length(b);
-  return atan2(s, c);
+  if (a == Point(0, 0) || b == Point(0, 0))
+    throw invalid_argument("angle: arguments should not be zero.");
+  return atan2(a * b, cross(a, b));
 }
--- geometry/point.test.cc
+++ geometry/point.test.cc
@@ -2,6 +2,41 @@
 #include <catch2/catch.hpp>
 #include "point.cc"
 
-TEST_CASE("Point works") {
-  REQUIRE(false);
+TEST_CASE("Point operations work") {
+  // ^
+  // |     R
+  // |
+  // | P
+  // |       Q
+  // + - - - - >
+  Point P(1, 2), Q(4, 1), R(3, 4);
+
+  SECTION("basic operators") {
+    REQUIRE(3 * P == Point(3, 6));
+    REQUIRE(P * Q == 6);
+  }
+
+  SECTION("string representation") {
+    ostringstream os; os << P;
+    REQUIRE(os.str() == "(1, 2)");
+  }
+
+  SECTION("cross product stuff") {
+    REQUIRE(cross(P, Q) == -7);
+
+    REQUIRE(ccw(P, Q, R) > 0);
+    REQUIRE(ccw(P, R, Q) < 0);
+    REQUIRE(ccw({1, 1}, {2, 2}, {4, 4}) == 0);
+
+    // TODO: Add area function
+  }
+
+  SECTION("length and angle") {
+    REQUIRE(Point(1, 0).length() == Approx(1));
+    REQUIRE(P.length() == Approx(sqrt(5)));
+
+    REQUIRE(angle({1, 0}, {1, 1}) == Approx(PI/4));
+    REQUIRE(angle(P, Q) == Approx(2.4329663815));
+    REQUIRE_THROWS_AS(angle({0, 0}, {1, 0}), invalid_argument);
+  }
 }
