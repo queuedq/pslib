d58822510008a49a4ed7f387dddda24247e3ad45
Dongkyu Han <queued37@gmail.com>
2020-10-05 21:11:38 +0900

    Test FFT

--- math/fft.cc
+++ math/fft.cc
@@ -3,24 +3,25 @@
 using cpx = complex<double>;
 const double PI = acos(-1);
 
-// source: https://blog.myungwoo.kr/54
-// re-written by me (queued_q)
+// Reference: https://blog.myungwoo.kr/54
 void fft(vector<cpx> &f, bool inv){
-  int n = f.size(), j = 0; // j: bit reverse of i
+  int n = f.size();
 
-  for (int i = 1; i < n; i++) { // permute to separate f_even and f_odd
-    int bit = (n >> 1);
-    for (; j >= bit; bit >>= 1) j -= bit;
+  // Rearrange coefficients to make f_even and f_odd form continuous intervals
+  for (int i=1, j=0; i<n; i++) {
+    int bit = n >> 1;
+    for (; j>=bit; bit>>=1) j -= bit;
     j += bit;
     if (i < j) swap(f[i], f[j]);
   }
 
-  for (int s = 2; s <= n; s <<= 1) { // length of each interval
+  // Divide and conquer
+  for (int s=2; s<=n; s<<=1) { // s: polynomial size
     double t = 2*PI / s * (inv ? -1 : 1);
-    cpx ws(cos(t), sin(t)); // w ** s == 1
-    for (int i = 0; i < n; i += s) { // start of each interval
+    cpx ws(cos(t), sin(t)); // s-th root of 1
+    for (int i=0; i<n; i+=s) {
       cpx w = 1;
-      for (int j = 0; j < s/2; j++) { // merge
+      for (int j=0; j<s/2; j++) { // merge
         cpx u = f[i+j], v = f[i+j + s/2] * w; // u: f_even, v: f_odd
         f[i+j] = u + v;
         f[i+j + s/2] = u - v;
@@ -30,21 +31,21 @@ void fft(vector<cpx> &f, bool inv){
   }
 
   if (inv) {
-    for (int i = 0; i < n; i++) f[i] /= n;
+    for (int i=0; i<n; i++) f[i] /= n;
   }
 }
 
-vector<cpx> multiply(vector<cpx> a, vector<cpx> b) {
+vector<lld> multiply(const vector<lld> &a, const vector<lld> &b) {
+  vector<cpx> fa(a.begin(), a.end()), fb(b.begin(), b.end());
   int n = 1;
-  while (n < max(a.size(), b.size())) n <<= 1;
-  n <<= 1;
-  a.resize(n); b.resize(n);
-  fft(a, false); fft(b, false);
+  while (n < max(a.size(), b.size()) * 2) n <<= 1;
+  fa.resize(n); fb.resize(n);
 
-  vector<cpx> c(n);
-  for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
-  fft(c, true);
-  return c;
+  fft(fa, false); fft(fb, false);
+  for (int i=0; i<n; i++) fa[i] *= fb[i];
+  fft(fa, true);
+
+  vector<lld> res(n);
+  for (int i=0; i<n; i++) res[i] = (lld)round(fa[i].real());
+  return res;
 }
-
-int cpxToInt(cpx z) { return (int)round(z.real()); }
--- math/fft.test.cc
+++ math/fft.test.cc
@@ -2,6 +2,11 @@
 #include <catch2/catch.hpp>
 #include "fft.cc"
 
-TEST_CASE("FFT works") {
-  REQUIRE(false);
+TEST_CASE("Polynomial multiplication works") {
+  vector<lld> P = {1, 4, 2};
+  vector<lld> Q = {2, 3, 5};
+  vector<lld> R = {1, 1, 1, 1};
+
+  REQUIRE(multiply(P, Q) == vector<lld>({2, 11, 21, 26, 10, 0, 0, 0}));
+  REQUIRE(multiply(R, R) == vector<lld>({1, 2, 3, 4, 3, 2, 1, 0}));
 }
