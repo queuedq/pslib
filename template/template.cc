b600886e7cae2eca94736a6e2195e93ad6cba2a5
Dongkyu Han <queued37@gmail.com>
2020-10-08 16:05:48 +0900

    Add main template

---
+++ template/template.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+using lld = long long;
+using pii = pair<int, int>;
+using pll = pair<lld, lld>;
+
+////////////////////////////////////////////////////////////////
+
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ////////////////////////////////
+
+
+
+  ////////////////////////////////
+  return 0;
+}
