0f047bd4e9469915b628c0a7c828c38d40363314
Han Dongkyu <queuedq@gmail.com>
2021-09-28 00:03:00 +0900

    Update DSU

--- data_structure/dsu.cc
+++ data_structure/dsu.cc
@@ -1,27 +1,19 @@
 #include <template.h>
 
-// Disjoint set union
-// 0-based index
 struct disjoint_set {
   vector<int> p, s;
 
   disjoint_set(int sz) {
     p.resize(sz); s.resize(sz);
-    for (int i = 0; i < sz; i++) {
-      p[i] = i;
-      s[i] = 1;
-    }
+    for (int i=0; i<sz; i++) { p[i] = i; s[i] = 1; }
   }
 
-  // Find the set number containing x
   int find(int x) {
     if (x == p[x]) return x;
     return p[x] = find(p[x]);
   }
 
-  // Merge two sets
-  // Note: "union" is a keyword in C++
-  void merge(int x, int y) {
+  void merge(int x, int y) { // note: "union" is a keyword in C++
     x = find(x); y = find(y);
     if (x == y) return;
     if (s[x] < s[y]) swap(x, y);
@@ -29,7 +21,7 @@ struct disjoint_set {
     s[x] += s[y];
   }
 
-  // Utility functions
+  // utility functions
   bool same(int x, int y) { return find(x) == find(y); }
   int size(int x) { return s[find(x)]; }
 };
