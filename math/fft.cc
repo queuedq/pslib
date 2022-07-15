d4ef40168ba67d42c8bc3d2610c765f0f3ed73dc
Han Dongkyu <queuedq@gmail.com>
2022-07-16 05:51:08 +0900

    Update FFT

--- math/fft.cc
+++ math/fft.cc
@@ -5,30 +5,31 @@
 #include <template.h>
 
 using cpx = complex<double>;
-const double PI = acos(-1);
+const double PI = acos(-1.L);
 
-// Part 1: sort by reversed-binary order
-// Part 2: simulate divide and conquer
+// Part 1: root memoization
+// Part 2: sort by reversed-binary order
+// Part 3: simulate divide and conquer
 void fft(vector<cpx> &f, bool inv){
-  int n = f.size();
-
-  for (int i=1, j=0; i<n; i++) {
-    int bit = n/2;
-    for (; j>=bit; bit/=2) j -= bit;
-    j += bit;
-    if (i < j) swap(f[i], f[j]);
+  int n = f.size(), L = 31 - __builtin_clz(n);
+  static vector<complex<long double>> R(2, 1);
+  static vector<cpx> rt(2, 1);
+  for (static int k=2; k<n; k*=2) {
+    R.resize(n); rt.resize(n);
+    auto x = polar(1.0L, acos(-1.0L) / k);
+    for (int i=k; i<2*k; i++) rt[i] = R[i] = (i&1) ? R[i/2] * x : R[i/2];
   }
 
-  for (int k=2; k<=n; k*=2) {
-    double t = 2*PI/k * (inv ? -1 : 1);
-    cpx z(cos(t), sin(t)); // z^k = 1
-    for (int i=0; i<n; i+=k) {
-      cpx zs = 1;
-      for (int j=0; j<k/2; j++) {
-        cpx u = f[i+j], v = f[i+j + k/2] * zs;
+  vector<int> rev(n);
+  for (int i=0; i<n; i++) rev[i] = (rev[i/2] | (i&1)<<L) / 2;
+  for (int i=0; i<n; i++) if (i < rev[i]) swap(f[i], f[rev[i]]);
+
+  for (int k=1; k<n; k*=2) {
+    for (int i=0; i<n; i+=2*k) {
+      for (int j=0; j<k; j++) {
+        cpx u = f[i+j], v = f[i+j+k] * (inv ? rt[j+k] : conj(rt[j+k]));
         f[i+j] = u + v;
-        f[i+j + k/2] = u - v;
-        zs *= z;
+        f[i+j+k] = u - v;
       }
     }
   }
@@ -40,7 +41,7 @@ void fft(vector<cpx> &f, bool inv){
 
 vector<lld> conv(const vector<lld> &a, const vector<lld> &b) {
   int m = sz(a) + sz(b) - 1;
-	int L = 32 - __builtin_clz(m), n = 1<<L;
+  int L = 32 - __builtin_clz(m), n = 1<<L;
 
   vector<cpx> f(all(a)), g(all(b));
   f.resize(n); g.resize(n);
@@ -56,7 +57,7 @@ vector<lld> conv(const vector<lld> &a, const vector<lld> &b) {
 
 vector<lld> conv_2in1(const vector<lld> &a, const vector<lld> &b) {
   int m = sz(a) + sz(b) - 1;
-	int L = 32 - __builtin_clz(m), n = 1<<L;
+  int L = 32 - __builtin_clz(m), n = 1<<L;
 
   vector<cpx> f(n), g(n), h(n);
   for (int i=0; i<sz(a); i++) h[i] += a[i];
@@ -78,7 +79,7 @@ vector<lld> conv_2in1(const vector<lld> &a, const vector<lld> &b) {
 
 vector<lld> conv_mod(const vector<lld> &a, const vector<lld> &b, lld M) {
   int m = sz(a) + sz(b) - 1;
-	int L = 32 - __builtin_clz(m), n = 1<<L;
+  int L = 32 - __builtin_clz(m), n = 1<<L;
   const lld cut = 1<<15;
 
   vector<cpx> f(n), g(n), ol(n), os(n);
@@ -95,10 +96,10 @@ vector<lld> conv_mod(const vector<lld> &a, const vector<lld> &b, lld M) {
 
   vector<lld> res(n);
   for (int i=0; i<n; i++) {
-		lld av = (lld)round(ol[i].real());
-		lld bv = (lld)round(ol[i].imag()) + (lld)round(os[i].real());
+    lld av = (lld)round(ol[i].real());
+    lld bv = (lld)round(ol[i].imag()) + (lld)round(os[i].real());
     lld cv = (lld)round(os[i].imag());
-		res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
+    res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
   }
   return res;
 }
