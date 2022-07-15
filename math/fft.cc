7a445138007dc12519279d9a6afa8196210a8a53
Han Dongkyu <queuedq@gmail.com>
2022-07-16 04:49:49 +0900

    Update FFT

--- math/fft.cc
+++ math/fft.cc
@@ -1,6 +1,7 @@
 // References
 // - https://blog.myungwoo.kr/54
 // - http://neerc.ifmo.ru/trains/toulouse/2017/fft2.pdf
+// - https://usaco.guide/adv/fft?lang=cpp
 #include <template.h>
 
 using cpx = complex<double>;
@@ -37,18 +38,67 @@ void fft(vector<cpx> &f, bool inv){
   }
 }
 
-vector<lld> multiply(const vector<lld> &a, const vector<lld> &b) {
-  vector<cpx> fa(all(a)), fb(all(b));
+vector<lld> conv(const vector<lld> &a, const vector<lld> &b) {
   int m = sz(a) + sz(b) - 1;
-	int L = 32 - __builtin_clz(m), n = 1 << L;
+	int L = 32 - __builtin_clz(m), n = 1<<L;
 
-  fa.resize(n); fb.resize(n);
+  vector<cpx> f(all(a)), g(all(b));
+  f.resize(n); g.resize(n);
 
-  fft(fa, 0); fft(fb, 0);
-  for (int i=0; i<n; i++) fa[i] *= fb[i];
-  fft(fa, 1);
+  fft(f, 0); fft(g, 0);
+  for (int i=0; i<n; i++) f[i] *= g[i];
+  fft(f, 1);
 
   vector<lld> res(n);
-  for (int i=0; i<n; i++) res[i] = (lld)round(fa[i].real());
+  for (int i=0; i<n; i++) res[i] = (lld)round(real(f[i]));
+  return res;
+}
+
+vector<lld> conv_2in1(const vector<lld> &a, const vector<lld> &b) {
+  int m = sz(a) + sz(b) - 1;
+	int L = 32 - __builtin_clz(m), n = 1<<L;
+
+  vector<cpx> f(n), g(n), h(n);
+  for (int i=0; i<sz(a); i++) h[i] += a[i];
+  for (int i=0; i<sz(b); i++) h[i] += cpx(0, b[i]);
+
+  fft(h, 0);
+  for (int i=0; i<n; i++) {
+    int j = -i & (n-1);
+    f[i] = (h[i] + conj(h[j])) / 2.;
+    g[i] = (h[i] - conj(h[j])) / 2i;
+  }
+  for (int i=0; i<n; i++) f[i] *= g[i];
+  fft(f, 1);
+
+  vector<lld> res(n);
+  for (int i=0; i<n; i++) res[i] = (lld)round(real(f[i]));
+  return res;
+}
+
+vector<lld> conv_mod(const vector<lld> &a, const vector<lld> &b, lld M) {
+  int m = sz(a) + sz(b) - 1;
+	int L = 32 - __builtin_clz(m), n = 1<<L;
+  const lld cut = 1<<15;
+
+  vector<cpx> f(n), g(n), ol(n), os(n);
+  for (int i=0; i<sz(a); i++) f[i] = cpx(a[i]/cut, a[i]%cut);
+  for (int i=0; i<sz(b); i++) g[i] = cpx(b[i]/cut, b[i]%cut);
+
+  fft(f, 0); fft(g, 0);
+  for (int i=0; i<n; i++) {
+    int j = -i & (n-1);
+    ol[i] = (f[i] + conj(f[j])) * g[i] / 2.;
+    os[i] = (f[i] - conj(f[j])) * g[i] / 2i;
+  }
+  fft(ol, 1); fft(os, 1);
+
+  vector<lld> res(n);
+  for (int i=0; i<n; i++) {
+		lld av = (lld)round(ol[i].real());
+		lld bv = (lld)round(ol[i].imag()) + (lld)round(os[i].real());
+    lld cv = (lld)round(os[i].imag());
+		res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
+  }
   return res;
 }
--- math/fft.test.cc
+++ math/fft.test.cc
@@ -6,7 +6,15 @@ TEST_CASE("Polynomial multiplication works") {
   vector<lld> P = {1, 4, 2};
   vector<lld> Q = {2, 3, 5};
   vector<lld> R = {1, 1, 1, 1};
+  vector<lld> S = {100001, 100001, 100001, 100001};
 
-  REQUIRE(multiply(P, Q) == vector<lld>({2, 11, 21, 26, 10, 0, 0, 0}));
-  REQUIRE(multiply(R, R) == vector<lld>({1, 2, 3, 4, 3, 2, 1, 0}));
+  REQUIRE(conv(P, Q) == vector<lld>({2, 11, 21, 26, 10, 0, 0, 0}));
+  REQUIRE(conv(R, R) == vector<lld>({1, 2, 3, 4, 3, 2, 1, 0}));
+
+  REQUIRE(conv_2in1(P, Q) == vector<lld>({2, 11, 21, 26, 10, 0, 0, 0}));
+  REQUIRE(conv_2in1(R, R) == vector<lld>({1, 2, 3, 4, 3, 2, 1, 0}));
+
+  REQUIRE(conv_mod(S, S, 1e9) == vector<lld>({
+    200001, 400002, 600003, 800004, 600003, 400002, 200001, 0
+  }));
 }
