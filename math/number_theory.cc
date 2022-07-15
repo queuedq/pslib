09128d01cad3853f49a034fb2d437e18baba0858
Han Dongkyu <queuedq@gmail.com>
2022-07-16 03:16:45 +0900

    Update number theory

--- math/number_theory.cc
+++ math/number_theory.cc
@@ -8,38 +8,46 @@ lld gcd(lld a, lld b) {
   return a;
 }
 
-pll egcd(lld a, lld b) {
-  lld ax = 1, ay = 0, bx = 0, by = 1;
-  while (b > 0) {
-    lld q = a / b;
-    a -= b * q; ax -= bx * q; ay -= by * q;
-    swap(a, b); swap(ax, bx); swap(ay, by);
-  }
-  return {ax, ay};
+lld egcd(lld a, lld b, lld &x, lld &y) {
+	if (!b) return x = 1, y = 0, a;
+	lld g = egcd(b, a%b, y, x);
+	return y -= a/b * x, g;
 }
 
-lld invmod(lld a, lld m) {
-  return (egcd(a, m).first + m) % m;
+lld modinv(lld a, lld m) {
+  lld x, y;
+  egcd(a, m, x, y);
+  return (x + m) % m;
 }
 
-lld powmod(lld a, lld e, lld m) {
-  if (e < 0) { a = invmod(a, m); e = -e; }
-  lld b = 1, f = 1;
-  while (f < e) f <<= 1;
-  while (f > 0) {
-    b = b * b % m;
-    if (e & f) b = b * a % m;
-    f >>= 1;
-  }
-  return b;
+lld modpow(lld b, lld e, lld m) {
+	lld r = 1;
+	for (; e; b = b*b % m, e/=2)
+		if (e&1) r = r*b % m;
+	return r;
 }
 
 lld phi(lld n) {
   lld res = n;
-  for (lld p = 2; p * p <= n; p++) {
-    if (n % p == 0) res = res / p * (p-1);
-    while (n % p == 0) n /= p;
+  for (lld p=2; p*p <= n; p++) {
+    if (n%p == 0) res = res / p * (p-1);
+    while (n%p == 0) n /= p;
   }
   if (n == 1) return res;
   return res / n * (n-1);
+}
+
+// x == r0 (mod m0), x == r1 (mod m1)
+// Returns {x, lcm(m0, m1)}
+pll crt(lld r0, lld m0, lld r1, lld m1) {
+  lld u, v;
+  egcd(m0, m1, u, v);
+  lld g = u*m0 + v*m1;
+  lld l = m0/g * m1;
+
+  if (r0 % g != r1 % g) return {0, 0}; // No solution
+
+  lld x = r0 + (r1-r0)/g * u*m0;
+  x = (x%l+l) % l;
+  return {x, l};
 }
--- math/number_theory.test.cc
+++ math/number_theory.test.cc
@@ -2,6 +2,31 @@
 #include <catch2/catch.hpp>
 #include "number_theory.cc"
 
-TEST_CASE("Number theory works") {
-  REQUIRE(false);
+TEST_CASE("gcd, egcd works") {
+  REQUIRE(gcd(21, 33) == 3);
+  lld x, y;
+  REQUIRE(egcd(21, 33, x, y) == 3);
+  REQUIRE(x == -3);
+  REQUIRE(y == 2);
+}
+
+TEST_CASE("modinv works") {
+  REQUIRE(modinv(13, 7) == 6);
+  REQUIRE(modinv(7, 13) == 2);
+}
+
+TEST_CASE("modpow works") {
+  REQUIRE(modpow(3, 5, 17) == 5);
+  REQUIRE(modpow(0, 5, 17) == 0);
+  REQUIRE(modpow(3, 0, 17) == 1);
+}
+
+TEST_CASE("phi works") {
+  REQUIRE(phi(504) == 144); // 2^3 * 3^2 * 7
+}
+
+TEST_CASE("CRT works") {
+  REQUIRE(crt(3, 5, 7, 9) == pll(43, 45));
+  REQUIRE(crt(4, 12, 10, 15) == pll(40, 60)); // gcd(12, 15) = 3
+  REQUIRE(crt(1, 12, 2, 15) == pll(0, 0)); // 1 !== 2 (mod 3)
 }
