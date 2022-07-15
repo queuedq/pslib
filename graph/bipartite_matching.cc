d07ed97cfa30e219454ea2ed0d8215e134ad9ad4
Han Dongkyu <queuedq@gmail.com>
2022-07-16 02:50:00 +0900

    Update bipartite matching

--- graph/bipartite_matching.cc
+++ graph/bipartite_matching.cc
@@ -3,52 +3,34 @@
 
 // 1-based index
 const int MN = 1005;
-int N, M;
+int N, M, S[MN], T[MN], vst[MN];
 vector<int> adj[MN];
 
-struct bipartite_matching {
-  int cnt, S[MN], T[MN], vst[MN];
-  // only for min vertex cover
-  int rvst[MN];
-  vector<int> lc, rc;
-
-  // max matching
-  bool match_node(int u) {
-    if (vst[u]) return 0;
-    vst[u] = 1;
-
-    for (int v: adj[u]) {
-      if (!T[v] || match_node(T[v])) {
-        S[u] = v; T[v] = u;
-        return 1;
-      }
-    }
-    return 0;
+bool match(int u) {
+  if (vst[u]) return 0;
+  vst[u] = 1;
+  for (auto v: adj[u]) {
+    if (!T[v] || match(T[v])) { S[u] = v, T[v] = u; return 1; }
   }
+  return 0;
+}
 
-  int match() {
-    for (int u=1; u<=N; u++) {
-      fill(vst+1, vst+N+1, 0);
-      cnt += match_node(u);
-    }
-    return cnt;
+// min vertex cover
+int rvst[MN];
+vector<int> lc, rc;
+
+void dfs(int u) {
+  if (vst[u]) return;
+  vst[u] = 1;
+  for (int v: adj[u]) {
+    rvst[v] = 1;
+    if (T[v]) dfs(T[v]);
   }
-
-  // min vertex cover
-  void dfs(int u) {
-    if (vst[u]) return;
-    vst[u] = 1;
-
-    for (int v: adj[u]) {
-      rvst[v] = 1;
-      if (T[v]) dfs(T[v]);
-    }
-  }
+}
   
-  void vertex_cover() {
-    fill(vst+1, vst+N+1, 0);
-    for (int u=1; u<=N; u++) { if (!S[u]) dfs(u); }
-    for (int u=1; u<=N; u++) { if (!vst[u]) lc.push_back(u); }
-    for (int v=1; v<=M; v++) { if (rvst[v]) rc.push_back(v); } // M, not N!
-  }
-};
+void vertex_cover() {
+  fill(vst+1, vst+N+1, 0);
+  for (int u=1; u<=N; u++) { if (!S[u]) dfs(u); }
+  for (int u=1; u<=N; u++) { if (!vst[u]) lc.push_back(u); }
+  for (int v=1; v<=M; v++) { if (rvst[v]) rc.push_back(v); } // M, not N!
+}
--- graph/bipartite_matching.test.cc
+++ graph/bipartite_matching.test.cc
@@ -4,20 +4,23 @@
 
 TEST_CASE("Bipartite matching works") {
   // Matching is unique for this example
-  vector<vector<int>> adj(5+1);
+  N = 5, M = 4;
   adj[1] = {1};
   adj[2] = {2, 3};
   adj[3] = {1, 2, 3, 5};
   adj[4] = {};
   adj[5] = {2};
 
-  bipartite_matching B(5, 5, adj);
-  B.match();
+  int cnt = 0;
+  for (int u=1; u<=N; u++) {
+    fill(vst+1, vst+N+1, 0);
+    cnt += match(u);
+  }
 
-  REQUIRE(B.cnt == 4);
-  REQUIRE(B.S[1] == 1);
-  REQUIRE(B.S[2] == 3);
-  REQUIRE(B.S[3] == 5);
-  REQUIRE(B.S[4] == 0);
-  REQUIRE(B.S[5] == 2);
+  REQUIRE(cnt == 4);
+  REQUIRE(S[1] == 1);
+  REQUIRE(S[2] == 3);
+  REQUIRE(S[3] == 5);
+  REQUIRE(S[4] == 0);
+  REQUIRE(S[5] == 2);
 }
