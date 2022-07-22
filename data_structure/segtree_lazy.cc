4b5912a3fc31a19ee2d8e8c0660a18cff9d555a9
Han Dongkyu <queuedq@gmail.com>
2022-07-22 18:10:28 +0900

    Update segtree lazyprop

--- data_structure/segtree_lazy.cc
+++ data_structure/segtree_lazy.cc
@@ -1,47 +1,45 @@
-// http://boj.kr/0666a2bbbb19489eb846633042505916
+// Range add range sum query, [l, r]
+// http://boj.kr/f64e25022c194d27a381ebb4dd064f9d
+// Reference: https://cp-algorithms.com/data_structures/segment_tree.html
 #include <template.h>
 
 const int ST = 1<<20; // 1 << (int)ceil(log2(N))
 lld seg[ST*2], lazy[ST*2];
 
-void propagate(int n, int nl, int nr) {
-  seg[n] += lazy[n] * (nr-nl+1);
-  if (n < ST) {
-    lazy[n*2] += lazy[n];
-    lazy[n*2+1] += lazy[n];
-  }
+void push(int n, int nl, int nr) {
+  seg[n*2] += lazy[n] * (nr-nl+1)/2; // sum query
+  seg[n*2+1] += lazy[n] * (nr-nl+1)/2;
+  lazy[n*2] += lazy[n];
+  lazy[n*2+1] += lazy[n];
   lazy[n] = 0;
 }
 
 void add_range(int n, int nl, int nr, int l, int r, lld v) {
-  propagate(n, nl, nr);
   if (nr < l || r < nl) return;
   if (l <= nl && nr <= r) {
+    seg[n] += v * (nr-nl+1); // sum query
     lazy[n] += v;
-    return propagate(n, nl, nr);
+    return;
   }
-
-  int mid = (nl+nr+1) / 2;
-  add_range(n*2, nl, mid-1, l, r, v);
-  add_range(n*2+1, mid, nr, l, r, v);
-  seg[n] = seg[n*2] + seg[n*2+1];
+  push(n, nl, nr);
+  int m = (nl+nr)/2;
+  add_range(n*2, nl, m, l, r, v);
+  add_range(n*2+1, m+1, nr, l, r, v);
+  seg[n] = seg[n*2] + seg[n*2+1]; // sum query
 }
+void add_range(int l, int r, lld v) { add_range(1, 0, ST-1, l, r, v); }
 
-lld query(int n, int nl, int nr, int l, int r) {
-  propagate(n, nl, nr);
+lld query_sum(int n, int nl, int nr, int l, int r) {
   if (nr < l || r < nl) return 0;
   if (l <= nl && nr <= r) return seg[n];
-
-  int mid = (nl+nr+1) / 2;
-  return query(n*2, nl, mid-1, l, r) + query(n*2+1, mid, nr, l, r);
+  push(n, nl, nr);
+  int m = (nl+nr)/2;
+  return query_sum(n*2, nl, m, l, r) + query_sum(n*2+1, m+1, nr, l, r); // sum query
 }
-
-// overloading
-void add_range(int l, int r, lld v) { add_range(1, 0, ST-1, l, r, v); }
-lld query(int l, int r) { return query(1, 0, ST-1, l, r); }
+lld query_sum(int l, int r) { return query_sum(1, 0, ST-1, l, r); }
 
 // usage
 void usage() {
   add_range(3, 6, 9); // A[3] += 9, ..., A[6] += 9
-  lld sum = query(4, 7); // sum = A[4] + ... + A[7]
+  lld sum = query_sum(4, 7); // sum = A[4] + ... + A[7]
 }
